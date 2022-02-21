3e749430e5dd71525c6c138cf6e68f435a21cd09
Kevin <tohkev@gmail.com>
2022-02-21 15:12:54 -0800

    organized initial structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+//route allows you to specify the route path to render a certain component
+//redirect allows you to redirect the path when the one specified is unknown
+//switch allows you to render only one route if that route applies (if none, then it will use redirect)
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Users />
+        </Route>
+
+        <Route path="/places/new" exact={true}>
+          <NewPlace />
+        </Route>
+
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewPlace() {
+  return <h2>New Place Content</h2>;
+}
+
+export default NewPlace;
