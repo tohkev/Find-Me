fbdf6c7d34149e985028182d05b0d5bae38b081f
Kevin <tohkev@gmail.com>
2022-02-21 21:19:59 -0800

    moved the drawer element using portals

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,9 +1,16 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import "./SideDrawer.css";
 
+//createPortal allows you to render a component outside of the usual 'root' element
 function SideDrawer(props) {
-  return <aside className="side-drawer">{props.children}</aside>;
+  const drawerContent = <aside className="side-drawer">{props.children}</aside>;
+
+  return ReactDOM.createPortal(
+    drawerContent,
+    document.getElementById("drawer-hook")
+  );
 }
 
 export default SideDrawer;
