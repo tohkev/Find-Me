0e76f6d0d0129989b5511bb017112112a459a185
Kevin <tohkev@gmail.com>
2022-02-22 21:49:00 -0800

    added sidebar functionality and animation

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -4,19 +4,34 @@ import { Link } from "react-router-dom";
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 import "./MainNavigation.css";
 
 //React.Fragment allows you to return two components that are side by side
 function MainNavigation(props) {
+  const [drawerIsOpen, setDrawerIsOpen] = React.useState(false);
+
+  function expandDrawerHandler() {
+    setDrawerIsOpen(true);
+  }
+
+  function closeDrawerHandler() {
+    setDrawerIsOpen(false);
+  }
+
   return (
     <React.Fragment>
-      <SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
       </SideDrawer>
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button
+          className="main-navigation__menu-btn"
+          onClick={expandDrawerHandler}
+        >
           <span></span>
           <span></span>
           <span></span>
--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
 import "./SideDrawer.css";
 
 //createPortal allows you to render a component outside of the usual 'root' element
+//react transition group allows you to animate transitions for components that are rendered/unrendered
 function SideDrawer(props) {
-  const drawerContent = <aside className="side-drawer">{props.children}</aside>;
+  const drawerContent = (
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer" onClick={props.onClick}>
+        {props.children}
+      </aside>
+    </CSSTransition>
+  );
 
   return ReactDOM.createPortal(
     drawerContent,
