1ea0c16aacb08ceba627986e1a2fb4e320c9e796
Kevin <tohkev@gmail.com>
2022-02-23 23:05:13 -0800

    added button styling

---
+++ frontend/src/shared/components/FormElements/Button.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Button.css";
+
+const Button = (props) => {
+  //if there is an href prop, render a link
+  if (props.href) {
+    return (
+      <a
+        className={`button button--${props.size || "default"} ${
+          props.inverse && "button--inverse"
+        } ${props.danger && "button--danger"}`}
+        href={props.href}
+      >
+        {props.children}
+      </a>
+    );
+  }
+
+  //if there is a to prop, we will render a link as it comes from react-router-dom
+  if (props.to) {
+    return (
+      <Link
+        to={props.to}
+        exact={props.exact}
+        className={`button button--${props.size || "default"} ${
+          props.inverse && "button--inverse"
+        } ${props.danger && "button--danger"}`}
+      >
+        {props.children}
+      </Link>
+    );
+  }
+  //if there is no to or href prop, a button is rendered
+  return (
+    <button
+      className={`button button--${props.size || "default"} ${
+        props.inverse && "button--inverse"
+      } ${props.danger && "button--danger"}`}
+      type={props.type}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
