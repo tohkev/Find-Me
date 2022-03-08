aa4026f13daf413a9f99188f5e1ed23a4b4327c2
Kevin <tohkev@gmail.com>
2022-03-07 21:51:23 -0800

    removed dummy data

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -5,22 +5,6 @@ const { validationResult } = require("express-validator");
 const getCoordsFromAddress = require("../util/location");
 const Place = require("../models/place");
 
-let DUMMY_PLACES = [
-	{
-		id: "p1",
-		title: "Empire State Building",
-		description: "One of the most famous skyscrapers in the world",
-		imageUrl:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
-		address: "20 W 34th St, New York, NY 10001",
-		creator: "u1",
-		location: {
-			lat: 40.7484405,
-			lng: -73.9878531,
-		},
-	},
-];
-
 async function getPlaceById(req, res, next) {
 	const placeId = req.params.pid;
 
