43b0942a2341c1a47a825cfa781f8768091d42c3
Kevin <tohkev@gmail.com>
2022-03-14 23:03:52 -0700

    revised image link

--- frontend/src/users/components/UserItem.js
+++ frontend/src/users/components/UserItem.js
@@ -7,23 +7,27 @@ import Card from "../../shared/components/UIElements/Card";
 
 //link wraps and renders an anchor tag that also blocks the navgation logic
 function UserItem(props) {
-  return (
-    <li className="user-item">
-      <Card className="user-item__content">
-        <Link to={`/${props.id}/places`}>
-          <div className="user-item__image">
-            <Avatar image={props.image} alt={props.name} />
-          </div>
-          <div className="user-item__info">
-            <h2>{props.name}</h2>
-            <h3>
-              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
-            </h3>
-          </div>
-        </Link>
-      </Card>
-    </li>
-  );
+	return (
+		<li className="user-item">
+			<Card className="user-item__content">
+				<Link to={`/${props.id}/places`}>
+					<div className="user-item__image">
+						<Avatar
+							image={`http://localhost:5000/${props.image}`}
+							alt={props.name}
+						/>
+					</div>
+					<div className="user-item__info">
+						<h2>{props.name}</h2>
+						<h3>
+							{props.placeCount}{" "}
+							{props.placeCount === 1 ? "Place" : "Places"}
+						</h3>
+					</div>
+				</Link>
+			</Card>
+		</li>
+	);
 }
 
 export default UserItem;
