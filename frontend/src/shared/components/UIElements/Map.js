97c8688e88b842c0e98e106a07ee70b1c01b100b
Kevin <tohkev@gmail.com>
2022-03-20 13:53:57 -0700

    switched to google maps api

--- frontend/src/shared/components/UIElements/Map.js
+++ frontend/src/shared/components/UIElements/Map.js
@@ -3,33 +3,48 @@ import React from "react";
 import "./Map.css";
 
 const Map = (props) => {
-  const mapRef = React.useRef();
+	// ***************If using openlayer instead of Google Maps **************
+	// const mapRef = React.useRef();
 
-  const { center, zoom } = props;
+	// const { center, zoom } = props;
 
-  React.useEffect(() => {
-    new window.ol.Map({
-      target: mapRef.current.id,
-      layers: [
-        new window.ol.layer.Tile({
-          source: new window.ol.source.OSM(),
-        }),
-      ],
-      view: new window.ol.View({
-        center: window.ol.proj.fromLonLat([center.lng, center.lat]),
-        zoom: zoom,
-      }),
-    });
-  }, [center, zoom]);
+	// React.useEffect(() => {
+	//   new window.ol.Map({
+	//     target: mapRef.current.id,
+	//     layers: [
+	//       new window.ol.layer.Tile({
+	//         source: new window.ol.source.OSM(),
+	//       }),
+	//     ],
+	//     view: new window.ol.View({
+	//       center: window.ol.proj.fromLonLat([center.lng, center.lat]),
+	//       zoom: zoom,
+	//     }),
+	//   });
+	// }, [center, zoom]);
 
-  return (
-    <div
-      ref={mapRef}
-      className={`map ${props.className}`}
-      style={props.style}
-      id="map"
-    ></div>
-  );
+	// ***************Using Google Maps API**************
+	const mapRef = React.useRef();
+
+	const { center, zoom } = props;
+
+	React.useEffect(() => {
+		const map = new window.google.maps.Map(mapRef.current, {
+			center: center,
+			zoom: zoom,
+		});
+
+		new window.google.maps.Marker({ position: props.center, map: map });
+	}, [center, zoom]);
+
+	return (
+		<div
+			ref={mapRef}
+			className={`map ${props.className}`}
+			style={props.style}
+			id="map"
+		></div>
+	);
 };
 
 export default Map;
